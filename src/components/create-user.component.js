08c0532e09f588c2b20ae57a2d90681ae3f13d59
Shoeb Tamboli <shoeb.tamboli@tesa.com>
2022-08-19 12:03:40 +0200

    update url

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,60 +1,66 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class CreateUser extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.onChangeUsername = this.onChangeUsername.bind(this);
-		this.onSubmit = this.onSubmit.bind(this);
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 
-		this.state = {
-			username: ''
-		};
-	}
+    this.state = {
+      username: "",
+    };
+  }
 
-	onChangeUsername(e) {
-		this.setState({
-			username: e.target.value
-		});
-	}
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
 
-	onSubmit(e) {
-		e.preventDefault();
+  onSubmit(e) {
+    e.preventDefault();
 
-		const user = {
-			username: this.state.username
-		};
+    const user = {
+      username: this.state.username,
+    };
 
-		console.log(user);
+    console.log(user);
 
-		axios.post('http://localhost:5000/users/add', user).then((res) => console.log(res.data));
+    axios
+      .post("https://mern-track-sample.herokuapp.com/users/add", user)
+      .then((res) => console.log(res.data));
 
-		this.setState({
-			username: ''
-		});
-	}
+    this.setState({
+      username: "",
+    });
+  }
 
-	render() {
-		return (
-			<div>
-				<h3>Create New User</h3>
-				<form onSubmit={this.onSubmit}>
-					<div className="form-group">
-						<label>Username: </label>
-						<input
-							type="text"
-							required
-							className="form-control"
-							value={this.state.username}
-							onChange={this.onChangeUsername}
-						/>
-					</div>
-					<div className="form-group">
-						<input type="submit" value="Create User" className="btn btn-primary" />
-					</div>
-				</form>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <h3>Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create User"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
